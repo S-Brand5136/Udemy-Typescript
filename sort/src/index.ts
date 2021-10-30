20d4e13bc2e81c79621c85abf959c463fdc3d2ee
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-10-30 12:44:36 -0600

    Refactor Sorter class to abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,20 @@
-import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumberCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumberCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
